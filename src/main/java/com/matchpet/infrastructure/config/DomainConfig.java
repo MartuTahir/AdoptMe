0d857c6ae7ab3398b3631f7f51cf7be3769a5159
Juan Manuel Torrejón <421483@tecnicatura.frc.utn.edu.ar>
2026-05-24 20:27:04 -0300

    Beans faltantes añadidos

---
+++ src/main/java/com/matchpet/infrastructure/config/DomainConfig.java
@@ -0,0 +1,26 @@
+package com.matchpet.infrastructure.config;
+
+import com.matchpet.domain.service.CompatibilityEngine;
+import com.matchpet.domain.service.ImpulsivityEngine;
+import com.matchpet.domain.service.TrustScoreCalculator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainConfig {
+
+    @Bean
+    public CompatibilityEngine compatibilityEngine() {
+        return new CompatibilityEngine();
+    }
+
+    @Bean
+    public ImpulsivityEngine impulsivityEngine() {
+        return new ImpulsivityEngine();
+    }
+
+    @Bean
+    public TrustScoreCalculator trustScoreCalculator() {
+        return new TrustScoreCalculator();
+    }
+}
